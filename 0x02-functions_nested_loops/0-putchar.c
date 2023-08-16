ecc47954a1f26c684e134c49e80dbddbd95211ba
MathiasPascal <pascal.mathias30@gmail.com>
2023-08-16 12:52:37 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,7 @@
  * Description: Prints _putchar and newline
  * Return: Always 0(success)
  */
-
-int main(void)	
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,5 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);	
+	return (0);
 }
