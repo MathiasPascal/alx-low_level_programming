0b8025a568631f70d5df172e0c05a3fcd5cd74a6
MathiasPascal <pascal.mathias30@gmail.com>
2023-08-16 15:39:06 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: checks for alphabetic character
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+
+int _isalpha(int c)
+
+{
+	
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+	return (1);
+	}
+	return (0);
+}
