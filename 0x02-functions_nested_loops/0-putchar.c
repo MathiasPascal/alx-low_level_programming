ef6b22568ff5f0dc51f4e5b5c563918465d8300f
MathiasPascal <pascal.mathias30@gmail.com>
2023-08-16 12:18:04 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,12 +3,13 @@
 /**
  * Main - Entry point 
  *
+ * Description: Prints _putchar and newline
+ *
  * Return: Always 0(success)
  */
 
 int main(void)
 {
-	_putchar('_');
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
