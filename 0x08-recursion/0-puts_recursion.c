fcd373d490e8b56d9f6b0ea67163d8dd1c5aa5d5
MathiasPascal <pascal.mathias30@gmail.com>
2023-09-09 10:00:46 -0700

    Betty style indent changes

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string.
+ * _puts_recursion - Prints a string
  *
  * @s: String
  */
 
 void _puts_recursion(char *s)
 {
-        if (*s)
-        {
-                _putchar(*s);
-                _puts_recursion(s + 1);
-        }
-        else
-        {
-                _putchar('\n');
-        }
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
